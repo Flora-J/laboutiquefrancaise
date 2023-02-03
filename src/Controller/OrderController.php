59410ae6b25be76c51cb305370b242901bff9fc0
Flora <flora.julie88@gmail.com>
2023-02-03 17:25:41 +0100

    Le paiement

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -66,6 +66,8 @@ class OrderController extends AbstractController
 
             // Enregistrer ma commande Order()
             $order = new Order();
+            $reference = $date->format('dmY').'-'.uniqid();
+            $order->setReference($reference);
             $order->setUser($this->getUser());
             $order->setCreatedAt($date);
             $order->setCarrierName($carriers->getName());
@@ -84,6 +86,7 @@ class OrderController extends AbstractController
                 $orderDetails->setPrice($product['product']->getPrice());
                 $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
                 $this->entityManager->persist($orderDetails);
+
             }
 
             $this->entityManager->flush();
@@ -91,7 +94,8 @@ class OrderController extends AbstractController
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
                 'carrier' => $carriers,
-                'delivery' => $delivery_content
+                'delivery' => $delivery_content,
+                'reference' => $order->getReference()
             ]);
         }
 
